74f24f0ac085369fa42abdaff1ae68d9bc828f02
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-27 16:52:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Cillian Wing';
 
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
